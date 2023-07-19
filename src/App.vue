8a3ffc703986f8cdfce2060ac8be94e433686923
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-20 00:28:56 +0600

    style binding

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <script setup>
-const bgOrange="bg-orange-400"
-const bgIndigo="bg-indigo-400"
-const bgGreen="bg-green-400"
+const bgOrange='background-color: rgba(251, 146, 60, 1)'
+const bgIndigo='background-color: rgba(129, 140, 248, 1)'
+const bgGreen='background-color: rgba(52,211,153,1)'
 
-//const bgRed="bg-red-600"
-const weekday=false
+const weekday=true
+const bgRed='background-color: rgba(220,38,38,1)'
+const bgDarkGreen='background-color: rgba(5,150,105,1)'
 
 const colors={
-    bgTeal:"bg-teal-100",
-    borderTeal:"border-teal-500",
-    textTeal:"text-teal-900"
+  borderColor:'rgba(20,184,166,1)',
+  color:'rgba(19,78,74,1)',
+  backgroundColor:'rgba(204,251,241,1)'
 }
-const messeageType="warning"
-if('error'==messeageType){
-  colors.bgTeal="bg-red-100"
-  colors.borderTeal="border-red-500",
-    colors.textTeal="text-red-900"
-}
-if('warning'==messeageType){
-  colors.bgTeal="bg-orange-100"
-  colors.borderTeal="border-orange-500",
-    colors.textTeal="text-orange-900"
+const messageType='error'
+ 
+if ('error'==messageType){
+  colors.borderColor='rgba(239, 68, 68,1)'
+  colors.color='rgba(127, 29, 29,1)'
+  colors.backgroundColor='rgba(254, 226, 226,1)'
 }
 
 </script>
@@ -30,23 +27,23 @@ if('warning'==messeageType){
         <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
 
         <section class="flex space-x-10">
-            <div class="w-1/3 h-16" :class="bgOrange"></div>
-            <div class="w-1/3 h-16" :class="bgIndigo"></div>
-            <div class="w-1/3 h-16" :class="bgGreen"></div>
+            <div class="w-1/3 h-16" :style="bgOrange"></div>
+            <div class="w-1/3 h-16" :style="bgIndigo"></div>
+            <div class="w-1/3 h-16" :style="bgGreen"></div>
         </section>
 
-        <section class="mt-10">
-            <div class="h-16 flex items-center justify-center text-white" :class="weekday?'bg-green-400':'bg-indigo-400'">
+       <section class="mt-10" >
+            <div class="h-16 flex items-center justify-center text-white" :style="[weekday? bgRed: bgDarkGreen,weekday?'font-size:20px':'font-size:10px']">
                 It's a weekday!
             </div>
-        </section>
+        </section> 
 
-        <section class="mt-10">
-            <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :class=[colors.bgTeal,colors.borderTeal,colors.textTeal] >
+        <section class="mt-10" >
+            <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :style="colors" >
                 <p class="font-bold">Our privacy policy has changed</p>
                 <p class="text-sm">Make sure you know how these changes affect you.</p>
             </div>
-        </section>
+        </section> 
     </section>
 </template>
 
