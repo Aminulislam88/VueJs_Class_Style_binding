3ecdc8c906bf9ac025dc12b9f4ff7a81dee5ceef
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-20 00:59:28 +0600

    basic loop done

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,14 @@
 <script setup>
-const bgOrange='background-color: rgba(251, 146, 60, 1)'
-const bgIndigo='background-color: rgba(129, 140, 248, 1)'
-const bgGreen='background-color: rgba(52,211,153,1)'
 
-const weekday=true
-const bgRed='background-color: rgba(220,38,38,1)'
-const bgDarkGreen='background-color: rgba(5,150,105,1)'
-
-const colors={
-  borderColor:'rgba(20,184,166,1)',
-  color:'rgba(19,78,74,1)',
-  backgroundColor:'rgba(204,251,241,1)'
-}
-const messageType='error'
- 
-if ('error'==messageType){
-  colors.borderColor='rgba(239, 68, 68,1)'
-  colors.color='rgba(127, 29, 29,1)'
-  colors.backgroundColor='rgba(254, 226, 226,1)'
-}
-
+const items=[1,2,3,4,,5,6]
 </script>
 
 <template>
-   <section class="container mx-auto">
-        <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
-
-        <section class="flex space-x-10">
-            <div class="w-1/3 h-16" :style="bgOrange"></div>
-            <div class="w-1/3 h-16" :style="bgIndigo"></div>
-            <div class="w-1/3 h-16" :style="bgGreen"></div>
-        </section>
-
-       <section class="mt-10" >
-            <div class="h-16 flex items-center justify-center text-white" :style="[weekday? bgRed: bgDarkGreen,weekday?'font-size:20px':'font-size:10px']">
-                It's a weekday!
-            </div>
-        </section> 
-
-        <section class="mt-10" >
-            <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :style="colors" >
-                <p class="font-bold">Our privacy policy has changed</p>
-                <p class="text-sm">Make sure you know how these changes affect you.</p>
-            </div>
-        </section> 
+  <body class="p-10">
+    <section class="mx-auto container flex items-center text-center flex-col space-y-2">
+        <div class="p-5 border border-gray-600 w-40" v-for="item in items">{{ item }}</div>
     </section>
+</body>
 </template>
 
 <style scoped>
