be11eeb989c5a527f7dc0e88b14549055407f98e
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-20 01:18:59 +0600

    2nd loop complete

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 
-const items=[1,2,3,4,,5,6]
+const tasks=[
+    "complete project proposel",
+    "Update website content",
+    "Fix bug in user authentication",
+    "prepare Presentation Slide",
+    "Software update"
+]
 </script>
 
 <template>
   <body class="p-10">
     <section class="mx-auto container flex items-center text-center flex-col space-y-2">
-        <div class="p-5 border border-gray-600 w-40" v-for="item in items">{{ item }}</div>
+        <div class="p-5 border border-gray-600 w-40" v-for="(task,index) in tasks">{{index +1}}-{{ task }}</div>
     </section>
 </body>
 </template>
