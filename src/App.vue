e8db6ce345986f887867ebec962e7daa2d450d55
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-20 14:28:23 +0600

    icon episode done

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,23 @@
 <script setup>
-const persons=[
-    {
-        id:1,
-        name:"Jane Doe",
-        profilePhoto:"https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg",
-        jobTitle:"CEO,Co-Founder"
-    },
-    {
-        id:2,
-        name:"Jhon Doe",
-        profilePhoto:"https://tailus.io/sources/blocks/classic/preview/images/man.jpg",
-        jobTitle:"CTO"
-    },
-    {
-        id:3,
-        name:"Jessy Doe",
-        profilePhoto:"https://tailus.io/sources/blocks/classic/preview/images/woman.jpg",
-        jobTitle:"COD"
-    }
+const icons=[
+    {title:"Sunny",icon:"https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png"},
+    {title:"Partly Cloudy",icon:"https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png"},
+    {title:"Rainy",icon:"https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png"},
+    {title:"Thunderstorm",icon:"https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png"}
 ]
 
 </script>
 
 <template>
 <body class="p-10">
-    <div class="py-20 bg-gray-50">
-        <div class="container mx-auto px-6 px-32">
-            <div class="mb-16 text-center">
-                <h2 class="mb-4 text-center text-2xl text-gray-900 font-bold text-4xl">Team Member</h2>
-                <p class="text-gray-600 w-8/12 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti veniam laudantium itaque perferendis dolor maxime incidunt voluptatem. </p>
-            </div>
-            <div class="grid gap-12 items-center grid-cols-3" >
-                <div class="space-y-4 text-center" v-for="person in persons" :key="person.id">
-                    <img class="w-64 h-64 mx-auto object-cover rounded-xl w-64 h-64" :src="person.profilePhoto">
-                    <div>
-                        <h4 class="text-2xl">{{person.name}}</h4>
-                        <span class="block text-sm text-gray-500">{{person.jobTitle}}</span>
-                    </div>
-                </div>  
-            </div>
-        </div>
-    </div>
+    <section class="mx-auto container">
+    <section class="mx-auto container space-y-3 p-5 border border-gray-500 w-80 flex flex-col items-center">
+        <template v-for="icon in icons" :key="icon.icon">
+        <h2 class="text-2xl">{{ icon.title }}</h2>
+        <img class="w-32" :src="icon.icon">
+    </template>
+    </section>
+    </section>
 </body>
 </template>
 
